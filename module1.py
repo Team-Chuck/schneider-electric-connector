f105708247550f85a7eeb1c168cac0871d405237
Jeff Bowman <jeff.bowman@schneider-electric.com>
2019-09-18 11:14:46 -0400

    added sample to set value

--- module1.py
+++ module1.py
@@ -1,8 +1,9 @@
 from ewsrestgatewayclient.ebo import EBO
+import urllib.parse
 
 
-test = EBO()
-token = test.token.get_token("Admin", "P@ssword")
-hmm = test.root.retrieve(custom_headers={'Authorization':'Bearer ' + token.access_token})
+se_client = EBO()
+token = se_client.token.get_token("Admin", "P@ssword")
+root_result = se_client.root.retrieve(custom_headers={'Authorization':'Bearer ' + token.access_token})
+set_value_result = se_client.values.update_value(urllib.parse.quote('01/Server 1/Cisco Hackathon/Team 4/Conference Room A/Values/Temp', safe=''), '28', custom_headers={'Authorization':'Bearer ' + token.access_token})
 test2 = 2
-
